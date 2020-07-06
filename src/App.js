c29b774f4e13d5a6dde6d8e4becd81c1507d7a1c
JDMTias <matiasiturbide@gmail.com>
2020-07-06 16:52:27 -0600

    starting out

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 
+
+const allTasks = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      allTasks: allTasks
+    }
+  }
+ 
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>My Todo's</h2>
       </div>
     );
   }
---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Todo = props => {
+    return (
+        <div>
+            {/* Remember to change this so its calling the right piece of infot it needs and delete the second <p> */}
+            {/* <p>{props.something.name}</p> */}
+            <p>Laundry</p>
+        </div>
+    )
+}
+
+export default Todo; 
